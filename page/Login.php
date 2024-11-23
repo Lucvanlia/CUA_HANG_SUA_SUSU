e8b31fb6296a4bf9cc31ee263a8427f287504199
LucVanLia <lethanhphata6@gmail.com>
2024-11-23 07:18:04 +0700

    frist commit

--- page/Login.php
+++
@@ -1,6 +0,0 @@
-<form action="page/submit_form.php" method="POST">
-    <!-- Các input khác của form -->
-    <div class="g-recaptcha" data-sitekey="6LdnOVMqAAAAAJkGZw5_DzR3vnPjkMrlP7OQawP8"></div>
-    <br/>
-    <input type="submit" value="Submit">
-</form>
--- page/config_vnpay.php
+++
@@ -1,6 +0,0 @@
-<?php
-$vnp_TmnCode = "1LOFCN1D"; // Mã website của bạn tại VNPAY
-$vnp_HashSecret = "APBU881NNPNVCUHTXA1WNVNJPGY9HERV"; // Chuỗi bí mật do VNPAY cung cấp
-$vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"; // URL thanh toán của VNPAY sandbox
-$vnp_Returnurl = "https://banhangviet-tmi.net/doan_php/page/vnpay_return.php"; // URL trả về sau khi thanh toán thành công
-?>
